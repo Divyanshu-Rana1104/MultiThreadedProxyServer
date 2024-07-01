23a24af71d4d0191b991c65e55d15e68301e64b9
Divyanshu_Rana_1104 <divyanshurana1104@gmail.com>
2024-07-01 12:19:17 +0530

    Cache Initialized

--- Proxy_server_with_cache.c
+++ Proxy_server_with_cache.c
@@ -1,7 +1,23 @@
 #include "proxy_parse.h"
-#include<stdio.h>
-#include<string.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <time.h>
+#include <pthread.h>
+
+
+#define MAX_CLIENTS 400     //max number of client requests served at a time
+
 typedef struct cache_element cache_element;
  struct cache_element{
     char* data;
@@ -11,4 +27,15 @@ typedef struct cache_element cache_element;
    struct cache_element* next;
 };
 
-cache
+cache_element* find(char* url);
+int add_cache_element(char* data,int size,char* url);
+void remove_cache_element();
+
+int port_number =8080;
+int proxy_socketID;
+pthread_t tid[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
+
+cache_element* head;
+int cache_size;
