7d93a60559d45ff9ff8b889a0b403b200496e310
Divyanshu_Rana_1104 <divyanshurana1104@gmail.com>
2024-07-05 13:10:12 +0530

    Data retrieval

--- Proxy_server_with_cache.c
+++ Proxy_server_with_cache.c
@@ -145,3 +145,155 @@ int handle_request(int clientSocket, ParsedRequest *request, char *tempReq)
 	strcat(buf, "\r\n");
 
 	size_t len = strlen(buf);
+
+if (ParsedHeader_set(request, "Connection", "close") < 0){
+		printf("set header key not work\n");
+	}
+
+	if(ParsedHeader_get(request, "Host") == NULL)
+	{
+		if(ParsedHeader_set(request, "Host", request->host) < 0){
+			printf("Set \"Host\" header key not working\n");
+		}
+	}
+
+	if (ParsedRequest_unparse_headers(request, buf + len, (size_t)MAX_BYTES - len) < 0) {
+		printf("unparse failed\n");
+		//return -1;				// If this happens Still try to send request without header
+	}
+
+	int server_port = 80;				// Default Remote Server Port
+	if(request->port != NULL)
+		server_port = atoi(request->port);
+
+	int remoteSocketID = connectRemoteServer(request->host, server_port);
+
+	if(remoteSocketID < 0)
+		return -1;
+
+	int bytes_send = send(remoteSocketID, buf, strlen(buf), 0);
+
+	bzero(buf, MAX_BYTES);
+
+	bytes_send = recv(remoteSocketID, buf, MAX_BYTES-1, 0);
+	char *temp_buffer = (char*)malloc(sizeof(char)*MAX_BYTES); //temp buffer
+	int temp_buffer_size = MAX_BYTES;
+	int temp_buffer_index = 0;
+
+	while(bytes_send > 0)
+	{
+		bytes_send = send(clientSocket, buf, bytes_send, 0);
+		
+		for(int i=0;i<bytes_send/sizeof(char);i++){
+			temp_buffer[temp_buffer_index] = buf[i];
+			// printf("%c",buf[i]); // Response Printing
+			temp_buffer_index++;
+		}
+		temp_buffer_size += MAX_BYTES;
+		temp_buffer=(char*)realloc(temp_buffer,temp_buffer_size);
+
+		if(bytes_send < 0)
+		{
+			perror("Error in sending data to client socket.\n");
+			break;
+		}
+		bzero(buf, MAX_BYTES);
+
+		bytes_send = recv(remoteSocketID, buf, MAX_BYTES-1, 0);
+
+	} 
+	temp_buffer[temp_buffer_index]='\0';
+	free(buf);
+	add_cache_element(temp_buffer, strlen(temp_buffer), tempReq);
+	printf("Done\n");
+	free(temp_buffer);
+	
+	
+ 	close(remoteSocketID);
+	return 0;
+}
+
+int checkHTTPversion(char *msg)
+{
+	int version = -1;
+
+	if(strncmp(msg, "HTTP/1.1", 8) == 0)
+	{
+		version = 1;
+	}
+	else if(strncmp(msg, "HTTP/1.0", 8) == 0)			
+	{
+		version = 1;										// Handling this similar to version 1.1
+	}
+	else
+		version = -1;
+
+	return version;
+}
+
+
+void* thread_fn(void* socketNew)
+{
+	sem_wait(&seamaphore); 
+	int p;
+	sem_getvalue(&seamaphore,&p);
+	printf("semaphore value:%d\n",p);
+    int* t= (int*)(socketNew);
+	int socket=*t;           // Socket is socket descriptor of the connected Client
+	int bytes_send_client,len;	  // Bytes Transferred
+
+	
+	char *buffer = (char*)calloc(MAX_BYTES,sizeof(char));	// Creating buffer of 4kb for a client
+	
+	
+	bzero(buffer, MAX_BYTES);								// Making buffer zero
+	bytes_send_client = recv(socket, buffer, MAX_BYTES, 0); // Receiving the Request of client by proxy server
+	
+	while(bytes_send_client > 0)
+	{
+		len = strlen(buffer);
+        //loop until u find "\r\n\r\n" in the buffer
+		if(strstr(buffer, "\r\n\r\n") == NULL)
+		{	
+			bytes_send_client = recv(socket, buffer + len, MAX_BYTES - len, 0);
+		}
+		else{
+			break;
+		}
+	}
+
+	// printf("--------------------------------------------\n");
+	// printf("%s\n",buffer);
+	// printf("----------------------%d----------------------\n",strlen(buffer));
+	
+	char *tempReq = (char*)malloc(strlen(buffer)*sizeof(char)+1);
+    //tempReq, buffer both store the http request sent by client
+	for (int i = 0; i < strlen(buffer); i++)
+	{
+		tempReq[i] = buffer[i];
+	}
+	
+	//checking for the request in cache 
+	struct cache_element* temp = find(tempReq);
+
+	if( temp != NULL){
+        //request found in cache, so sending the response to client from proxy's cache
+		int size=temp->len/sizeof(char);
+		int pos=0;
+		char response[MAX_BYTES];
+		while(pos<size){
+			bzero(response,MAX_BYTES);
+			for(int i=0;i<MAX_BYTES;i++){
+				response[i]=temp->data[pos];
+				pos++;
+			}
+			send(socket,response,MAX_BYTES,0);
+		}
+		printf("Data retrived from the Cache\n\n");
+		printf("%s\n\n",response);
+		// close(socketNew);
+		// sem_post(&seamaphore);
+		// return NULL;
+	}
+	
+	
